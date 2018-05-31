eea5f8b00f80ae7ee4d0dd09bd21267db9bc59e6
Rohit Sharma <rohit@socialweaver.co>
2018-05-31 23:01:54 +0530

    entry point for the library created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import Add from './helpers';
 
-console.log(Add(10, 530));
+const Node = function (props) {
+  const body = '<div></div>';
+}
+
+const addChild = (parent, body) => {
+
+  if (Array.isArray(parent.children)) {
+    parent.children.forEach(child => {
+      body += '*' + addChild(child, '');
+    });
+  } else {
+    body += ',' + parent.name
+  }
+  return body;
+}
+
+(() => {
+  window.iTree = function (container) {
+    this.draw = (config) => {
+      console.log(config, container);
+
+      container.innerHTML = '<button></button>'
+
+      const node = new Node('abcd');
+      console.log(node.name, node.myName);
+
+      const t = addChild(config, '');
+      console.log(t);
+    };
+  }
+})()
